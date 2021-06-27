b97ae7ff9b1b4462ba163269633039a581fecf91
PQuagliatto <pquagliatto17@gmail.com>
2021-06-27 19:07:54 -0300

    heroku deployment is ready

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path= require('path')
+const express = ('express')
+
+const port =  process.env.PORT || 8080
+
+const app = express()
+
+const publicPath = path.join(__dirname, '..', 'build')
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'))
+})
+
+app.listen(port, () => {
+  console.log('server is running')
+})
